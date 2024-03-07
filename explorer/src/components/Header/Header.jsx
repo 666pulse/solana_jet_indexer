5313f0cc69f0256ccdc60ef502f99467c2b606df
liushooter <shooter.hero@gmail.com>
2024-03-07 09:58:07 +0800

    layout

---
+++ explorer/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useRef, useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+import { Breadcrumb, Menu, theme } from 'antd';
+import { Space, Table, Tag, Layout } from 'antd';
+import axios, {isCancel, AxiosError} from 'axios';
+
+
+export default function MainHeader() {
+  const { Header } = Layout;
+
+  const items = new Array(5).fill(null).map((_, index) => ({
+    key: index + 1,
+    label: `nav ${index + 1}`,
+  }));
+
+  return (
+    <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+  );
+}
