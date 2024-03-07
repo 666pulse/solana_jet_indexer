629bdebb6719c414eed5a48d4a2b846cad544389
liushooter <shooter.hero@gmail.com>
2024-03-07 09:05:37 +0800

    table

--- explorer/src/App.js
+++ explorer/src/App.js
@@ -1,12 +1,85 @@
 import React from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-
+import { Breadcrumb, Menu, theme } from 'antd';
+import { Space, Table, Tag, Layout } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 const items = new Array(5).fill(null).map((_, index) => ({
   key: index + 1,
   label: `nav ${index + 1}`,
 }));
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
 const App = () => {
   const {
@@ -61,7 +134,7 @@ const App = () => {
             borderRadius: borderRadiusLG,
           }}
         >
-          Content
+           <Table columns={columns} dataSource={data} />
         </div>
       </Content>
 
@@ -70,7 +143,7 @@ const App = () => {
           textAlign: 'center',
         }}
       >
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        DePINscan ©{new Date().getFullYear()} Created by Ant UED
       </Footer>
     </Layout>
   );
