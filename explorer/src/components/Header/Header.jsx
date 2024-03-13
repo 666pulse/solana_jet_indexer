d174ff6c97c07875bb73d1d71b43929787f5cfbf
liushooter <shooter.hero@gmail.com>
2024-03-13 14:38:26 +0800

    nav

--- explorer/src/components/Header/Header.jsx
+++ explorer/src/components/Header/Header.jsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { useRef, useEffect, useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
 
-import { Breadcrumb, Menu, theme } from 'antd';
-import { Space, Table, Tag, Layout } from 'antd';
-import axios, {isCancel, AxiosError} from 'axios';
+import { Breadcrumb, Menu, Layout } from 'antd';
+import { Space, Table, Tag, theme } from 'antd';
+
+// const items = new Array(5).fill(null).map((_, index) => ({
+//   key: index + 1,
+//   label: `nav ${index + 1}`,
+// }));
+
+const items = [
+  {
+    label: (
+      <a href="https://www.bing.com/" target="_blank" rel="noopener noreferrer">
+        Bing
+      </a>
+    ),
+    key: 'bing',
+  },
+  {
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Ant Design
+      </a>
+    ),
+    key: 'antdesign',
+  },
+]
+
 
 
 export default function MainHeader() {
   const { Header } = Layout;
-
-  const items = new Array(5).fill(null).map((_, index) => ({
-    key: index + 1,
-    label: `nav ${index + 1}`,
-  }));
 
   return (
     <Header
@@ -22,12 +41,14 @@ export default function MainHeader() {
           alignItems: 'center',
         }}
       >
-        <div className="demo-logo" />
+        <div className="demo-logo">
+          <img src={process.env.PUBLIC_URL + '/logo.svg'} />;
+        </div>
 
         <Menu
           theme="dark"
           mode="horizontal"
-          defaultSelectedKeys={['2']}
+          defaultSelectedKeys={['1']}
           items={items}
           style={{
             flex: 1,
