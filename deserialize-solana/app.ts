9b739999d99b436fff6ddbac2dd76140fff78a93
liushooter <shooter.hero@gmail.com>
2024-03-15 15:55:17 +0800

    up

--- deserialize-solana/app.ts
+++ deserialize-solana/app.ts
@@ -2,7 +2,10 @@ import { PublicKey, Connection } from "@solana/web3.js";
 import { u32, u8, struct } from "@solana/buffer-layout";
 import { publicKey, u64, bool } from "@solana/buffer-layout-utils";
 
+// https://www.helius.dev/blog/solana-dev-101-deserializing-account-data-on-solana
 // https://www.quicknode.com/guides/solana-development/accounts-and-data/how-to-deserialize-account-data-on-solana
+// https://github.com/solana-labs/solana-program-library/blob/master/token/js/src/state/mint.ts
+// https://borsh.m2.xyz/
 
 export interface RawMint {
   mintAuthorityOption: 1 | 0;
@@ -25,7 +28,7 @@ export const MintLayout = struct<RawMint>([
 ]);
 
 async function fetchAndParse(mint: PublicKey, solanaConn: Connection) {
-  console.log(`Step - 1: Fetching Account Data for ${mint.toBase58()}`);
+  console.log(`Step - 1: Fetching Account Data`);
   let data = await solanaConn.getAccountInfo(mint);
   if (!data) {
     console.log(`data is null`);
@@ -54,10 +57,12 @@ async function fetchAndParse(mint: PublicKey, solanaConn: Connection) {
   console.log("Freeze Authority:", deserialized.freezeAuthority.toString());
 }
 
-const mainrpc = "https://api.helius.xyz";
+const mainrpc = "";
+// const devrpc = "";
 const conn = new Connection(mainrpc);
 
 const addr = "7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU";
+
 const pk = new PublicKey(addr);
 
 fetchAndParse(pk, conn);
